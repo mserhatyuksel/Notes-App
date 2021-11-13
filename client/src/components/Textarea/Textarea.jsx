f4dc2e30027e813572cde7f3d39b6d2f57b67c25
mserhatyuksel <mserhat.yksl@gmail.com>
2021-11-13 16:21:47 +0300

    input type changed to radio

--- client/src/components/Textarea/Textarea.jsx
+++ client/src/components/Textarea/Textarea.jsx
@@ -1,28 +1,34 @@
-import React from "react";
 import styles from "./Textarea.module.css";
 const TextArea = () => {
+  const handleChecked = (e) => console.log(e.target.id);
   return (
     <div className={styles.inputWrapper}>
-      <textarea className={styles.textArea} name="note" cols="100" rows="7" placeholder="Enter your note here..." />
-      <div className={styles.colors}>
+      <textarea
+        className={styles.textArea}
+        name="note"
+        cols="100"
+        rows="7"
+        placeholder="Enter your note here..."
+      />
+      <div className={styles.colors} onChange={handleChecked}>
         <label className={styles.container}>
-          <input type="checkbox" />
+          <input type="radio" name="color" id="pink" />
           <span className={`${styles.checkmark} ${styles.pink}`} />
         </label>
         <label className={styles.container}>
-          <input type="checkbox" />
+          <input type="radio" name="color" id="purple" />
           <span className={`${styles.checkmark} ${styles.purple}`} />
         </label>
         <label className={styles.container}>
-          <input type="checkbox" />
+          <input type="radio" name="color" id="yellow" />
           <span className={`${styles.checkmark} ${styles.yellow}`} />
         </label>
         <label className={styles.container}>
-          <input type="checkbox" />
+          <input type="radio" name="color" id="blue" />
           <span className={`${styles.checkmark} ${styles.blue}`} />
         </label>
         <label className={styles.container}>
-          <input type="checkbox" />
+          <input type="radio" name="color" id="green" />
           <span className={`${styles.checkmark} ${styles.green}`} />
         </label>
         <button className={styles.add_btn}>ADD</button>
