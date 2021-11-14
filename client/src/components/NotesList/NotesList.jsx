7b160c3f47a7b79c3e02578b71e5c0d34f238228
mserhatyuksel <mserhat.yksl@gmail.com>
2021-11-14 17:16:34 +0300

    Added grid

--- client/src/components/NotesList/NotesList.jsx
+++ client/src/components/NotesList/NotesList.jsx
@@ -1,11 +1,16 @@
 import styles from "./NotesList.module.css";
 const NotesList = ({ notes }) => {
   return (
-    <div className="list">
+    <div className={styles["grid-container"]}>
       {notes.map((note) => {
         return (
-          <div key={note.id} className={styles[`${note.color}`]}>
-            <span>{note.text}</span>
+          <div
+            key={note.id}
+            className={`${styles[`${note.color}`]} ${styles.note} ${
+              styles["grid-item"]
+            }`}
+          >
+            {note.text}
           </div>
         );
       })}
